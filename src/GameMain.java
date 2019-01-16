a56936d7afda7633f86ab5ac4a7e5d33a6b2c27d
Gregory Karastergios <gregory@gregnk.com>
2019-01-16 14:07:37 -0500

    Updated code header

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,13 +1,14 @@
 /*
  * Name(s): Gregory Karastergios
- * Date: 2019-01-19
+ * Date: 2019-01-18
  * Course code: ICS4U1-01, Mr. Fernandes
  * Title: Due Process
  * Description: A trial simulation game
- * Major skills: Recursion, OOP
- * Features: 
+ * Major skills: Recursion, OOP, Algorithms
+ * Features: User input processing, 
  * Areas of concern: 
- *
+ * 	* Polymorhisim could have been used with the GUI classes
+ * 	* The game framework took longer than expected to make, therefore the cases were somewhat rushed
  */
 
 public class GameMain {
