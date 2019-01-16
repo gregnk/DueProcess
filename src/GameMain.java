1cfd617a26af5377e6d4c5cad4a5c5f23a2a1383
Gregory Karastergios <gregory@gregnk.com>
2019-01-15 22:23:22 -0500

    Added code header (incomplete)

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,3 +1,14 @@
+/*
+ * Name(s): Gregory Karastergios
+ * Date: 2019-01-19
+ * Course code: ICS4U1-01, Mr. Fernandes
+ * Title: Due Process
+ * Description: A trial simulation game
+ * Major skills: Recursion, OOP
+ * Features: 
+ * Areas of concern: 
+ *
+ */
 
 public class GameMain {
 
